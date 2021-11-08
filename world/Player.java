14faa7fec1f06dc45db19dfe450840e37a6a1eea
wanghr <239955612@qq.com>
2021-11-08 19:31:33 +0800

    init

---
+++ world/Player.java
@@ -0,0 +1,11 @@
+package world;
+
+import java.awt.Color;
+
+public class Player extends Creature {
+    public Player(World world, char glyph, Color color, int maxHP, int attack, int defense, int visionRadius) {
+        super(world, glyph, color, maxHP, attack, defense, visionRadius);
+    }
+
+    int digCount = 0;
+}
