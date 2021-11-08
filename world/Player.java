ce62901e01c126bb29feb559be0fc7ffde2f292f
wanghr <239955612@qq.com>
2021-11-08 20:14:23 +0800

    add id2Color

--- world/Player.java
+++ world/Player.java
@@ -8,4 +8,25 @@ public class Player extends Creature {
     }
 
     public int digCount = 0;
+
+    public static Color id2Color(int id) {
+        switch (id) {
+        case 0:
+            return Color.LIGHT_GRAY;
+        case 1:
+            return Color.ORANGE;
+        case 2:
+            return Color.YELLOW;
+        case 3:
+            return Color.RED;
+        case 4:
+            return Color.BLUE;
+        case 5:
+            return Color.GREEN;
+        case 6:
+            return Color.CYAN;
+        default:
+            return Color.LIGHT_GRAY;
+        }
+    }
 }
