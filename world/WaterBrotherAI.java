17269458a16759dcdde85a7841c8bf2304fe6530
wanghr <239955612@qq.com>
2021-11-13 14:59:00 +0800

    add attack

--- world/WaterBrotherAI.java
+++ world/WaterBrotherAI.java
@@ -11,6 +11,27 @@ public class WaterBrotherAI extends PlayerAI {
     @Override
     public void run() {
         onSkill = true;
+        int r = 5;
+        int x = player.x();
+        int y = player.y();
+        Creature enemy = null;
+        for (int i = 0; i < r; ++i)
+            for (int j = 0; j < r - i; ++j) {
+                if (i == 0 && j == 0)
+                    continue;
+                enemy = world.creature(x + j, y + i);
+                if (enemy != null)
+                    player.attack(enemy);
+                enemy = world.creature(x + j, y - i);
+                if (enemy != null)
+                    player.attack(enemy);
+                enemy = world.creature(x - j, y + i);
+                if (enemy != null)
+                    player.attack(enemy);
+                enemy = world.creature(x - j, y - i);
+                if (enemy != null)
+                    player.attack(enemy);
+            }
         try {
             Thread.sleep(1000);
         } catch (Exception e) {
