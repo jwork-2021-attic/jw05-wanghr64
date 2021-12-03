da4a5af43b17b8c2db2cab329266ea7224dc1085
wanghr <239955612@qq.com>
2021-12-03 10:49:05 +0800

    add validAI

--- world/Player.java
+++ world/Player.java
@@ -1,13 +1,19 @@
 package world;
 
 import java.awt.Color;
+import java.util.Arrays;
 
 public class Player extends Creature {
     public Player(World world, char glyph, Color color, int maxHP, int attack, int defense, int visionRadius) {
         super(world, glyph, color, maxHP, attack, defense, visionRadius);
+        this.validAIs = new boolean[7];
+        Arrays.fill(this.validAIs, false);
+        this.validAIs[0] = true;
     }
 
     public int digCount = 0;
+
+    public boolean[] validAIs;
 
     public boolean onSkill() {
         return ai.onSkill();
@@ -15,22 +21,22 @@ public class Player extends Creature {
 
     public static Color id2Color(int id) {
         switch (id) {
-        case 0:
-            return Color.LIGHT_GRAY;
-        case 1:
-            return Color.ORANGE;
-        case 2:
-            return Color.YELLOW;
-        case 3:
-            return Color.RED;
-        case 4:
-            return Color.BLUE;
-        case 5:
-            return Color.GREEN;
-        case 6:
-            return Color.CYAN;
-        default:
-            return Color.LIGHT_GRAY;
+            case 0:
+                return Color.LIGHT_GRAY;
+            case 1:
+                return Color.ORANGE;
+            case 2:
+                return Color.YELLOW;
+            case 3:
+                return Color.RED;
+            case 4:
+                return Color.BLUE;
+            case 5:
+                return Color.GREEN;
+            case 6:
+                return Color.CYAN;
+            default:
+                return Color.LIGHT_GRAY;
         }
     }
 
